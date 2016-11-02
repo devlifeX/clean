0d6d6a135da5764a53692c66867c843874a5635e
Dariush vesal <dariush.vesal@gmail.com>
2016-11-02 22:22:26 +0330

    Add clean functions

--- app.rb
+++ app.rb
@@ -10,11 +10,52 @@ extensions = {
   executable: ['exe', 'sh','msi','bat'],
 }
 
+
+
 directories = {
+  myFolders: ['folder'],
   myImage: ['image'],
   myVideo: ['video'],
   myMusic: ['audio'],
   myCode: ['language', 'executable'],
   myOffice: ['office'],
-  myArchive: ['archive']
+  myArchive: ['archive'],
 }
+
+@myPath = '/home/devlife/Desktop/tmp/';
+
+def dir_handler(dirs)
+  dirs.each do |dir, group|
+    if !Dir.exist?(@myPath + dir.to_s)
+      dir_make(dir.to_s)
+    end
+  end
+end
+
+
+def dir_make(dir)
+  Dir.mkdir(@myPath + dir)
+end
+
+def get_all_files
+  return Dir[@myPath + "*"]
+end
+
+def clean_move(file)
+ if File.directory?(file)
+  
+ end 
+
+end
+
+def clean_handler(dir, exe) 
+ get_all_files.each do |file|
+  clean_move(file.to_s)
+end
+end
+
+
+
+# dir_handler(directories)
+
+clean_handler(directories, extensions)
