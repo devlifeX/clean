2a576d6a7b80c4d43d77a3dc571d3c17de5a3c34
Dariush vesal <dariush.vesal@gmail.com>
2016-11-02 21:58:51 +0330

    Add config

--- app.rb
+++ app.rb
@@ -1,1 +1,20 @@
 #!/usr/bin/ruby
+
+extensions = {
+  image: ['jpg','png'],
+  audio: ['mp3'],
+  video: ['mp4', 'mkv','3gp','mpeg'],
+  office: ['xls', 'xlsx','doc','docx','ppt','pptx'],
+  archive: ['zip', 'tar','tar.gz','tar.bz','rar'],
+  language: ['rb', 'py','php','js','css','less','sass'],
+  executable: ['exe', 'sh','msi','bat'],
+}
+
+directories = {
+  myImage: ['image'],
+  myVideo: ['video'],
+  myMusic: ['audio'],
+  myCode: ['language', 'executable'],
+  myOffice: ['office'],
+  myArchive: ['archive']
+}
